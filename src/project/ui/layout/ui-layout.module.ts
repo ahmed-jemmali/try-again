1919704a84b05bee68762b979f71d61ff4055a93
ahmed-jm <127752640+ahmed-jm@users.noreply.github.com>
2023-10-30 23:07:25 +0100

    add header main footer

---
+++ src/project/ui/layout/ui-layout.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UiLayoutRoutingModule } from './ui-layout-routing.module';
+import { MainComponent } from './main/main.component';
+import { HeaderToolbarComponent } from './header-toolbar/header-toolbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    MainComponent,
+    HeaderToolbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    UiLayoutRoutingModule
+  ]
+})
+export class UiLayoutModule { }
